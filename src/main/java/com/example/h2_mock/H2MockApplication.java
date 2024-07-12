21fa133d8bb89422d9f08a429b05299a218d73c9
ebielle <eryblongo@gmail.com>
2024-07-12 20:58:33 +0200

    exercise

---
+++ src/main/java/com/example/h2_mock/H2MockApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2MockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2MockApplication.class, args);
+	}
+
+}
